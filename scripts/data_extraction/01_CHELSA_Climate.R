1e42538c6c840ac0ceec4844097e1d73647b58ea
Elise Gallois <elisg@nhm-28yf2l3.nhm.ac.uk>
2024-09-24 11:42:11 +0100

    Elise starts CHELSA data extraction

---
+++ scripts/data_extraction/01_CHELSA_Climate.R
@@ -0,0 +1,10 @@
+# A script for extracting historic, contemporary, and projected CHELSA data
+# To be stored on the NHM Valentine server under the 'biodivhealth' directory
+
+# 1. Libraries & Setup ----
+
+
+# 2. Initialise download ----
+
+
+# 3. Run and save download ----
